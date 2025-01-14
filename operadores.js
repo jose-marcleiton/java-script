fb2d1c0425420436773c59e7163d5f7e165270ee
Your Name <j.m.a.m7@hotmail.comgit config -- user.name jose-marcleitongit config --global user.email j.m.a.m7@hotmail.com>
2025-01-13 22:08:36 -0300

    primeira aula de js

---
+++ operadores.js
@@ -0,0 +1,17 @@
+// let inteiro = 10
+// console.log(inteiro)
+// let soma = inteiro + 2
+// let subtracao = inteiro - 2
+// let divisao = inteiro / 2
+
+// let prontoFloat = 10.5
+
+// let sumString = '10.5'
+// let SomaString = numString + 2
+// let SubtracaoString = numerString - 2
+// let divisaoString = numString / 2
+
+let salario = prompt('qual o seu salario')
+salario = Number(salario)
+salario = salario + 1000
+console.log(salario)
---
+++ script.js
@@ -0,0 +1,51 @@
+// arquivo s=externo de js
+
+// funções de output
+// mostrar informações na tela do navegador
+
+console.log('Olá Mundo!')
+console.log("Olá Mundo!")
+// alert('olá mundo')
+
+// funções de input
+// prompt - capta uma informação digitada pelo usuário no formato de texto
+// confirm- capta uma informação digitada pelo usuario no formato de booleano
+
+// variáveis
+// let x = 10
+// console.log(x)
+// x = 100
+// console.log(x)
+
+
+var y = 20
+console.log(y)
+var y = 'minha senha'
+console.log(y)
+
+
+// const pi = 3.14
+// console.log(pi)
+// pi = 'minha senha'
+
+// let x = 20
+// console.log(x)
+
+let nome = 'marcleiton'
+let idade = 29
+let email = 'marcleiton@gmail.com'
+const dataNascimento = '27/06/1995'
+
+console.log('nome é '+ nome)
+console.log('A idade é' + idade)
+console.log('O email é' + email)
+console.log(' A dataNascimento é'+dataNascimento)
+
+// let nomeUduario = prompt("nome de usuario")
+// console.log(nomeUsuario)
+
+// let minhaIdade = prompt("idade")
+// console.log(minhaIdade)
+
+let MeuEmail = prompt('nomedoEmail')
+console.log(MeuEmail)
