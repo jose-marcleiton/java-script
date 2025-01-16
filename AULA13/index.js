4e8f8beb566006b98ec23a0460e2dd5c6b2d7062
Your Name <j.m.a.m7@hotmail.comgit config -- user.name jose-marcleitongit config --global user.email j.m.a.m7@hotmail.com>
2025-01-15 21:56:39 -0300

    condicionais e testes logicos

---
+++ AULA13/index.js
@@ -0,0 +1,64 @@
+
+
+
+// let numero = 2
+
+// if(numero ==0){
+//     console.log('zero')
+// }
+// else if (numero ==1){
+//     console.log('dois')
+// }
+// else{
+//     console.log('numero desconhecido')
+// }
+
+// atividade de aula
+
+// let num1 = Number(prompt('digite o primeiro numero'))
+// let num2 = Number(prompt('digite o segundo numero'))
+
+// if(num1 > num2){
+// alert('o primeiro numero é maior que o segundo')
+// }
+
+
+// else if(num2 > num1){
+//     alert('o segundo numero é maior que o primeiro')
+// }
+
+// else{
+// alert('os numero são iguais')
+// }
+
+// condição de paridade
+
+// let num = 7.2
+// console.log(num / 2)
+// console.log(num % 2)
+// console.log((num % 2) == 0)
+
+// if(num % 2 == 0){
+//     console.log('par')
+// }
+
+// else {
+//     console.log ('impar')
+// }
+
+// console.log(Number('palavra'))
+
+// let num = Number(prompt('digite um numero'))
+
+// if(isNaN(num)){
+//     console.log('digite apenas numeros')
+// }
+// else if (num%2 == 0){
+//     num = num + 5
+//     console.log (num)
+// }
+
+// else {
+//     num = num + 8
+//     console.log = (num)
+// }
---
+++ AtividadeAula12.js
@@ -0,0 +1,20 @@
+// soma , subtração, mais divisão
+
+
+let n1 = prompt('digite a primeira nota')
+let n2 = prompt('digite a segunda nota')
+let n3 = prompt('digite a terceira nota')
+n1 = Number(n1)
+n2 = Number(n2)
+n3 = Number(n3)
+
+let media = (n1 + n2 + n3) / 3
+
+
+alert('sua média é' + media)
+
+
+
+
+
+
